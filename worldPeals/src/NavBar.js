3741a8f9c543cda3b577b3aab2eaff1a4e47269f
Hamzat Abdulaleem <hamzatabdulaleem123@gmail.com>
2024-04-25 12:21:23 -0700

    adding new db to code

--- worldPeals/src/NavBar.js
+++ worldPeals/src/NavBar.js
@@ -3,11 +3,13 @@ const NavBar = () => {
         <div className="Navbar">
             <h1>world peas</h1>
             <div className="links">
-                <a href="#">shop</a>
-                <a href="#">newstand</a>
-                <a href="#">who we are</a>
-                <a href="#">my profile</a>
-                <a href="#">basket</a>
+                <a href="/">Shop</a>
+                <a href="/">Newstand</a>
+                <a href="/">Who we are</a>
+                <a href="/">My profile</a>
+            </div>
+            <div className="carton-button">
+                <a href="/">Basket</a>
             </div>
         </div>
      );
--- worldPeals/src/home.js
+++ worldPeals/src/home.js
@@ -1,6 +1,28 @@
 const Home = () => {
     return ( 
-        <h1>hello</h1>
+       <div className="home">
+          <div className="intro-text">
+          <h2>We’re farmers, purveyors, and eaters of organically grown food.</h2>
+          <button>Browse our shop</button>
+        </div>
+        <div className="images">
+          <div className="img1"></div>
+          <div className="text-img">
+          <div className="img2"></div>
+          <h3><p>Central California</p> The person who grew these was located in Central California and, er, hopefully very well-compensated.</h3>
+          </div>
+        </div>
+        <div className="text">
+          <h2>WHAT WE BELIEVE</h2>
+          <article>
+            We believe in produce. Tasty produce. Produce like: <br /> <br />
+            Apples. Oranges. Limes. Lemons. Guavas. Carrots. Cucumbers. Jicamas. Cauliflowers. Brussels sprouts. Shallots. Japanese eggplants. Asparagus. Artichokes—Jerusalem artichokes, too. Radishes. Broccoli. Baby broccoli. Broccolini. Bok choy. Scallions. Ginger. Cherries. Raspberries. Cilantro. Parsley. Dill. <br /><br />
+            What are we forgetting?
+            <br /><br />
+            Oh! Onions. Yams. Avocados. Lettuce. Arugula (to some, “rocket”). Persian cucumbers, in addition to aforementioned “normal” cucumbers. Artichokes. Zucchinis. Pumpkins. Squash (what some cultures call pumpkins). Sweet potatoes and potato-potatoes. Jackfruit. Monk fruit. Fruit of the Loom. Fruits of our labor (this website). Sorrel. Pineapple. Mango. Gooseberries. Blackberries. Tomatoes. Heirloom tomatoes. Beets. Chives. Corn. Endive. Escarole, which, we swear, we’re vendors of organic produce, but if you asked us to describe what escaroles are...
+          </article>
+        </div>
+       </div>
      );
 }
  
